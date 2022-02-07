fd0024dd60f2c6adaa32d62094ece46fd3ffd00e
mariucas <mariana.bolgar@gmail.com>
2022-02-07 21:35:43 +0200

    modificare

--- calc.js
+++ calc.js
@@ -15,68 +15,17 @@ function clearAll () {
     finish=false;
     out.textContent = 0;
 }
-   document.querySelector ('.ac').onclik = clearAll;
+   document.querySelector('.ac').onclik = clearAll;
 
-   document.querySelector ('.buttons').onclik=(Event)=> {
-       if(Event.target.classList.contains('btn')) return;
-
+   document.querySelector('.buttons').onclik = (Event) => {
+       if(!Event.target.classList.contains('btn')) return;
        if(Event.target.classList.contains('ac')) return;
    }
    
    out.textContent = '';
    const key = Event.target.textContent;
 
-   if (digit.includes(key)) {
-       if (b ==='' && sign ==='') {
-           a += key;
-           out.textContent = a;
-       }
-        else if (a!==''&& b!==''&& finish) {
-            b = key;
-            finish = false;
-            out.textContent = b;
-        }
-        else{
-            b == key;
-            out.textContent = b;
-        }
-        console.table(a,b , sign);
-        return;
-    }
-
-
-        if ( action.includes(key)) {
-            sign = key;
-            out.textContent = sign;
-            console.table(a,b,sign);
-            return;
-        }
-        if (key === '='){
-            if (b=== '') b = a;
-            switch (sign){
-                case "+":
-                    a=(+a) + (+b);
-                    break;
-                    case"-":
-                    a = a - b;
-                    break;
-                    case"X":
-                    a = a * b;
-                    break;
-                    case "/":
-                        if (b === '0'){
-                            out.textContent = 'Greseala';
-                            a= '';
-                            b= '';
-                            sign = '';
-                            return;
-                        }
-             a = a /b;
-             break;
-            }
-            finish = true;
-            out.textContent = a;
-            console.table(a, b, sign);
-        }
-   
-   
+  if(digit.includes(key)) {
+      a+=key;
+      console.log(a, b, sign);
+  }
