89a2b0b0230fc98465f8d1a1fa78d9b51cf49c72
mariucas <mariana.bolgar@gmail.com>
2022-02-08 23:22:04 +0200

    modific href

--- calc.js
+++ calc.js
@@ -1,32 +1,88 @@
-let a = '';
-let b ='';
-let sign ='';
-let finish = false;
+let a = ''; // first number
+let b = ''; // secont number
+let sign = ''; // знак операции
+let finish  = false;
 
-const digit = [ '0','1','2','3','4','5','6','7','8','9','.'];
-const action = ['-', '+','X','/'];
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
 
+// экран 
 const out = document.querySelector('.calc-screen p');
 
 function clearAll () {
-    a ='';
-    b ='';
-    sign ='';
+    a = ''; // first number and result
+    b = ''; // second number 
+    sign = ''; // знак
     finish = false;
     out.textContent = 0;
 }
-   document.querySelector('.ac').onclik = clearAll ;
+document.querySelector('.ac').onclick = clearAll;
 
-   document.querySelector('.buttons').onclik = (Event) => {
-       if(!EventTarget.classList.contains('btn')) return;
-       if(EventTarget.classList.contains('ac')) return;
-   
-   
-   out.textContent = '';
-   const key = EventTarget.textContent;
-   
-  if (digit.includes(key)) {
-      a += key;
-      console.log(a, b, sign);
-  }
+document.querySelector('.buttons').onclick = (event) => {
+    // нажата не кнопка
+    if(!event.target.classList.contains('btn')) return;
+    // нажата кнопка clearAll ac
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    // получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    // если нажата клавиша 0-9 или .
+    if (digit.includes(key)) {
+        if (b ==='' && sign === '') {
+            a += key;
+            
+            out.textContent = a;
+        }
+        else if (a!=='' && b!=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b , sign);
+        return;
+    }
+
+     // если нажата клавиша + - / *
+     if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b , sign);
+        return;
+    }
+
+    // нажата =
+    if (key === '=') {
+        if (b ==='') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "X":
+                a = a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b , sign);
+    }
+
 }
